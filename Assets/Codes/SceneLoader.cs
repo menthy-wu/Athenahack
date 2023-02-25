d30d527fd85d1dcd2606be6c4caa68aaba733444
ysh0305 <yhung022@ucr.edu>
2023-02-25 14:04:23 -0800

    create menu

---
+++ Assets/Codes/SceneLoader.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+}
